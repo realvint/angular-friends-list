8928ea68d0b464fb37c951fffb2bdd2c6e2b1552
realvint <victor.malukhin@gmail.com>
2020-11-07 16:15:54 +0300

    Lesson 5. Course completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from '@angular/material/list';
 import { UsersListComponent } from 'src/app/users-list/users-list.component';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { UsersListComponent } from 'src/app/users-list/users-list.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatListModule
+    FormsModule,
+    MatListModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from "../users.service";
+import {User} from "../shared/user";
+import {MatListOption} from "@angular/material/list";
 
 @Component({
   selector: 'app-users-list',
@@ -8,11 +10,46 @@ import {UsersService} from "../users.service";
 })
 export class UsersListComponent implements OnInit {
 
-  usersList = []
+  usersList: User [] = []
+  username: string
+  name: string
+  role: string
+  selectedList: User []
   constructor(public usersService: UsersService) { }
 
   ngOnInit(): void {
-this.usersList = this.usersService.getUsersList()
+    this.usersList = this.usersService.getUsersList()
+  }
+  search(query: string) {
+    this.usersList = this.usersService.findUser(query)
+  }
+  sort(direction: string) {
+    this.usersList = this.usersService.sortUser(direction)
+  }
+
+  addUsers() {
+
+      this.usersService.addUser({
+      id: Math.floor((Math.random() * 20) + 10),
+      name: this.name,
+      username: this.username,
+      email: "",
+      role: this.role,
+      phone: "",
+      website: ""
+    })
+    this.usersList = this.usersService.getUsersList()
+  }
+  selectItem(item: MatListOption []) {
+    this.selectedList = []
+    item.forEach(element => {
+      this.selectedList.push(element.value)
+    })
+  }
+  delUsers() {
+    this.usersService.delUser(this.selectedList)
+
+    this.usersList = this.usersService.getUsersList()
   }
   }
 
